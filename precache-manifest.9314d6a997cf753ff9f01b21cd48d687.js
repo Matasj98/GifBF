ebdc5e768d78f3c583d4058f495835b59f078fd4
Matasj98 <jari.matas@gmail.com>
2020-03-22 18:02:28 +0200

    Updates

---
+++ precache-manifest.9314d6a997cf753ff9f01b21cd48d687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46749f33e11005f62cff134d26f2510d",
+    "url": "/Matasj98/GifBF/index.html"
+  },
+  {
+    "revision": "238c168e01ef67f9cb42",
+    "url": "/Matasj98/GifBF/static/css/main.d939642b.chunk.css"
+  },
+  {
+    "revision": "19a6559e75b64936db41",
+    "url": "/Matasj98/GifBF/static/js/2.33022d06.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Matasj98/GifBF/static/js/2.33022d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "238c168e01ef67f9cb42",
+    "url": "/Matasj98/GifBF/static/js/main.a4525e99.chunk.js"
+  },
+  {
+    "revision": "f29e110b710ca89cc01d",
+    "url": "/Matasj98/GifBF/static/js/runtime-main.ab021126.js"
+  }
+]);
